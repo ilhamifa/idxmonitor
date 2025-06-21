3f7765927c77fd879bf447be374e82df63d27be9
ilhamifa <ilham.filardhi13@gmail.com>
2025-06-21 22:28:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,14 +16,17 @@ app.get("/get", async (req, res) => {
   const url = `https://www.idnfinancials.com/id/${symbol}`;
   try {
     const response = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          req.headers["user-agent"] ||
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/125 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml",
-      },
-      timeout: 8000,
-    });
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9",
+    "Connection": "keep-alive"
+  },
+  timeout: 8000
+});
+
 
     res.send(response.data);
   } catch (err) {

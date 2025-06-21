6637594b9e072b78fe97dfb87875618c7e11a623
ilhamifa <ilham.filardhi13@gmail.com>
2025-06-21 23:54:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,15 +13,12 @@ app.get("/get", async (req, res) => {
   const symbol = req.query.symbol;
   if (!symbol) return res.status(400).send("Symbol is required");
 
-  let fullSymbol = symbol;
-let region = "ID";
+ let fullSymbol = symbol;
+let region = "US";
 
 if (!symbol.includes(".")) {
-  // If user provides just 'BBCA', add '.JK' and use Indonesia
   fullSymbol = `${symbol}.JK`;
-} else {
-  // Use US region for US stocks
-  region = "US";
+  region = "ID";
 }
 
   const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${fullSymbol}&region=${region}`;
